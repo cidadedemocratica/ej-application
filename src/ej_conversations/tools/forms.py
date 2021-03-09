8d54cd000e07ad141e9cba9862d2180c95b844d9
Fabíola Fleury <fabiola.mfleury@gmail.com>
2021-03-09 18:13:55 -0300

    Update template changing select fields to radio buttons

--- src/ej_conversations/tools/forms.py
+++ src/ej_conversations/tools/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-AUTH_TYPE_CHOICES =( 
-    ("register", _("Register using email")), 
-    ("mautic", _("Mautic")), 
+AUTH_TYPE_CHOICES = (
+    ("register", _("Register using email")),
+    ("mautic", _("Mautic")),
     ("analytics", _("Analytics")),
 )
 
-THEME_CHOICES =( 
-    ("", _("Default")), 
-    ("votorantim", _("Votorantim")), 
+THEME_CHOICES = (
+    ("", _("Default")),
+    ("votorantim", _("Votorantim")),
     ("icd", _("ICD")),
 )
 
-class ConversationComponentForm(forms.Form): 
-    authentication_type = forms.ChoiceField(label=_("Authentication Type"), choices=AUTH_TYPE_CHOICES, required=False)
-    theme = forms.ChoiceField(label=_("Theme"), choices=THEME_CHOICES, required=False) 
+
+class ConversationComponentForm(forms.Form):
+    authentication_type = forms.ChoiceField(
+        label=_("Authentication Type"), choices=AUTH_TYPE_CHOICES,
+        required=False, widget=forms.RadioSelect
+    )
+    theme = forms.ChoiceField(
+        label=_("Theme"), choices=THEME_CHOICES,
+        required=False, widget=forms.RadioSelect
+    )
