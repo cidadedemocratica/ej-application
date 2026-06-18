e9123014934f42a01438ec2380411189be4ab67b
Fábio Teixeira <fabio1079@gmail.com>
2026-06-18 01:06:28 +0000

    Restore local dev environment

--- src/ej/settings/__init__.py
+++ src/ej/settings/__init__.py
@@ -39,6 +39,9 @@ class Conf(
     HOSTNAME = env("localhost")
 
     STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
         "staticfiles": {
             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
